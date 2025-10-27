0b9bf7186c356c4ee4de01af366e16eac3899142
Daniel <dkoz10035@gmail.com>
2025-10-27 22:31:47 +0100

    Wstępny schemat panelu Admina

--- Projekt-Siłownia/AdminPage.xaml.cs
+++ Projekt-Siłownia/AdminPage.xaml.cs
@@ -11,4 +11,39 @@ public partial class AdminPage : ContentPage
     {
         Application.Current.MainPage = new NavigationPage(new LoginPage());
     }
+    private async Task ShowOnlyAsync(VisualElement layoutToShow)
+    {
+        // Znajdź wszystkie layouty na stronie
+        var layouts = new[] { layout1, layout2, layout3 };
+
+        // Ukryj te, które są inne
+        foreach (var layout in layouts)
+        {
+            if (layout == layoutToShow)
+                continue;
+
+            if (layout.IsVisible)
+            {
+                await layout.FadeTo(0, 200); // 200ms
+                layout.IsVisible = false;
+            }
+        }
+
+        // Pokaż wybrany layout
+        if (!layoutToShow.IsVisible)
+        {
+            layoutToShow.Opacity = 0;
+            layoutToShow.IsVisible = true;
+            await layoutToShow.FadeTo(1, 200);
+        }
+    }
+
+    private async void OnLayout1Clicked(object sender, EventArgs e)
+        => await ShowOnlyAsync(layout1);
+
+    private async void OnLayout2Clicked(object sender, EventArgs e)
+        => await ShowOnlyAsync(layout2);
+
+    private async void OnLayout3Clicked(object sender, EventArgs e)
+        => await ShowOnlyAsync(layout3);
 }
