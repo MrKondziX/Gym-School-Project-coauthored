661a7b5061d7353fe311a779008e14c976a622a7
Daniel <dkoz10035@gmail.com>
2025-10-28 14:18:52 +0100

    Dodawanie Trenera

--- Projekt-Siłownia/AuthService.cs
+++ Projekt-Siłownia/AuthService.cs
@@ -42,6 +42,36 @@ namespace Projekt_Siłownia
             await _context.SaveChangesAsync();
             return "Zarejestrowano Pomyślnie";
         }
+        public async Task<string> AddCoach(string usersName, string usersSurname, string usersEmail, string usersLogin, string usersPassword)
+        {
+            if (await _context.Users.AnyAsync(u => u.UsersLogin == usersLogin))
+                return "Zajęty Login";
+
+            var user = new User
+            {
+                UsersId = 0,
+                UsersName = usersName,
+                UsersSurname = usersSurname,
+                UsersEmail = usersEmail,
+                UsersLogin = usersLogin,
+                UsersPassword = usersPassword,
+                UsersTypeId = 2
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            var createdUser = await _context.Users.FirstOrDefaultAsync(u => u.UsersLogin == usersLogin); // trzeba wydobyć ID z bazy danych, gdyż do działania AUTO_INCREMENT trzeba bazie danych dać ID 0, chyba że ktoś będzie miał lepszy pomysł niż to
+
+            var userCoach = new UsersCoach
+            {
+                UsersCoachId = 0,
+                UsersId = createdUser.UsersId,
+                UsersCoachNott = "Trener Personalny"
+            };
+            _context.UsersCoaches.Add(userCoach);
+            await _context.SaveChangesAsync();
+            return "Dodano trenera Pomyślnie";
+        }
 
         public async Task<(string result, int? userType, int? UserId)> Login(string usersLogin, string usersPassword)
         {
